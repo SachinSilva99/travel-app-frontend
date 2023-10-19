2c861faf8a095036040203d45e3364fe89d8260a
SachinSilva99 <sachinmanathunga@gmail.com>
2023-10-19 19:14:24 +0530

    created vehicle admin ui

---
+++ vehicle-admin/main.js
@@ -0,0 +1,39 @@
+function handleImageChange(inputElement, imageElement, imageSectionElement) {
+    inputElement.addEventListener('change', function (event) {
+        if (event.target.files && event.target.files[0]) {
+            const reader = new FileReader();
+            reader.onload = function (e) {
+                imageElement.src = e.target.result;
+                imageSectionElement.style.display = 'block';
+            };
+
+            reader.readAsDataURL(event.target.files[0]);
+        } else {
+            imageElement.src = '';
+            imageSectionElement.style.display = 'none';
+        }
+    });
+}
+
+handleImageChange(
+    document.getElementById('vehicleMainImage'),
+    document.getElementById('vehicleMainPic'),
+    document.getElementById('vehicleMainPicSection')
+);
+
+handleImageChange(
+    document.getElementById('vehicleImgFront'),
+    document.getElementById('vehicleFrontPic'),
+    document.getElementById('vehicleFrontPicSection')
+);
+
+handleImageChange(
+    document.getElementById('vehicleImgBack'),
+    document.getElementById('vehicleBackPic'),
+    document.getElementById('vehicleBackPicSection')
+);
+handleImageChange(
+    document.getElementById('vehicleImgBackInterior'),
+    document.getElementById('vehicleBackInteriorPic'),
+    document.getElementById('vehicleBackInteriorPicSection')
+);
---
+++ vehicle-admin/model/VehicleDTO.js
@@ -0,0 +1,21 @@
+export class VehicleDTO {
+    constructor(vehicleId, vehicleBrand, vehicleCategory, vehicleFuelType, vehicleFuelConsumption, vehicleIsHybrid, vehicleNoOfSeats, vehicleType, vehicleTransmission, vehicleRemarks, vehicleDriverName, vehicleDriverContact, vehicleMainImage, vehicleImgFront, vehicleImgBack, vehicleImgFrontInterior, vehicleImgBackInterior) {
+        this.vehicleId = vehicleId;
+        this.vehicleBrand = vehicleBrand;
+        this.vehicleCategory = vehicleCategory;
+        this.vehicleFuelType = vehicleFuelType;
+        this.vehicleFuelConsumption = vehicleFuelConsumption;
+        this.vehicleIsHybrid = vehicleIsHybrid;
+        this.vehicleNoOfSeats = vehicleNoOfSeats;
+        this.vehicleType = vehicleType;
+        this.vehicleTransmission = vehicleTransmission;
+        this.vehicleRemarks = vehicleRemarks;
+        this.vehicleDriverName = vehicleDriverName;
+        this.vehicleDriverContact = vehicleDriverContact;
+        this.vehicleMainImage = vehicleMainImage;
+        this.vehicleImgFront = vehicleImgFront;
+        this.vehicleImgBack = vehicleImgBack;
+        this.vehicleImgFrontInterior = vehicleImgFrontInterior;
+        this.vehicleImgBackInterior = vehicleImgBackInterior;
+    }
+}
